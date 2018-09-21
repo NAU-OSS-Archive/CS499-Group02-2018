20014675548a778165c8685ca6adcf1c40022576
Kalen <kalenw.dev@gmail.com>
2018-09-20 22:29:08 -0700

    Fixed compiler warnings

--- assignmentPR/MathFunc2.c
+++ assignmentPR/MathFunc2.c
@@ -15,21 +15,20 @@ bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 
 bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
-    
+
 }
 
 
 char answer[100];
-int c1, n1, d1;
-int c2, n2, d2;
+
  
-c1 = 1;
-n1 = 1;
-d1 = 2;
+int c1 = 1;
+int n1 = 1;
+int d1 = 2;
  
-c2 = 2;
-n2 = 2;
-d2 = 3;
+int c2 = 2;
+int n2 = 2;
+int d2 = 3;
 
 int main(void)
 {
