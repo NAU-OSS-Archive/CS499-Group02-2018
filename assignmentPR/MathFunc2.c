efb19f2a61c90f0d7d530de428a2bba7cbed6a42
Kalen <kalenw.dev@gmail.com>
2018-09-20 22:23:01 -0700

    Added skeleton code for assignment

---
+++ assignmentPR/MathFunc2.c
@@ -0,0 +1,46 @@
+typedef int bool;
+#define true 1
+#define false 0
+
+
+bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+
+// Code Here
+
+bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
+{
+
+}
+
+bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
+{
+    
+}
+
+
+char answer[100];
+int c1, n1, d1;
+int c2, n2, d2;
+ 
+c1 = 1;
+n1 = 1;
+d1 = 2;
+ 
+c2 = 2;
+n2 = 2;
+d2 = 3;
+
+int main(void)
+{
+    //if the C string could hold at least the characteristic
+    if(add(c1, n1, d1, c2, n2, d2, answer, 100))
+    {
+        //display string with answer 4.166666666...
+    }
+    else
+    {
+        //display error message
+    }
+
+}
