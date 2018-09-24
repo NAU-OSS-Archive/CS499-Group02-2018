df6bad258fa7fa14994474b87a1a96047bde7ba9
Kalen <kalenw.dev@gmail.com>
2018-09-23 18:59:27 -0700

    Minor text fixes

--- assignmentPR/MathFunc1.c
+++ assignmentPR/MathFunc1.c
@@ -1,7 +1,7 @@
 /* 
 
 MathFunc01.c
-Written by Stephen White & Kalen Wood
+Written by Stephen White, Kalen Wood-Wardlow, & Gary Baker
 9/23/18
 
 */
@@ -177,20 +177,6 @@ bool mantissa(char numString[], int* numerator, int *denominator)
 
     return true; 
 }
-
-
-/*
-
-The two functions, characteristic() and mantissa(), 
-will break a character array into a characteristic and a mantissa. The characteristic for 
-the number 2.351 is 2 and the mantissa is 351 over 1,000. The characteristic for the number 
-0.0125 is 0 and the mantissa is 125 over 10,000. The characteristic for the number -4.0 is -4 and 
-the mantissa is 0 over 10. These values should be stored in the reference parameters c, numerator and
-denominator. The C string may include leading or trailing spaces, unary plus or minus signs, integers,
-or real numbers. Your functions must handle all of these cases. If an invalid string is passed in your
-function will return false. If the passed in string was valid return true.
-
-*/
 
 
 /* Utility Functions */
